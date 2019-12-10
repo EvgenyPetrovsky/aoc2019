b87105341b9a0e6102320f162eb40991018cf45c
Evgeny Petrovskiy <ev.petrovsky@gmail.com>
2019-12-10 19:09:23 +0100

    day 4 part 2

--- R/day04.R
+++ R/day04.R
@@ -10,7 +10,7 @@ day04_makesequence <- function(low, high) {
 }
 
 #' Function that generalizes 2 types of checks using binary check operator and
-#' 
+#'
 #' function that folds outcomes to say whether check passes or not
 #' @param number value to analyze
 #' @param check_fun function to apply to check values
@@ -63,5 +63,49 @@ day04_part1_solution <- function() {
   ns <- strsplit(aoc19::DATASET$day04, split = "-",fixed = T)[[1]]
   rs <- day04_makesequence(ns[1], ns[2]) %>%
     Filter(f = function(x) all(day04_filterasc(x), day04_filteradj(x)))
+  length(rs)
+}
+
+#' Check whether adjacent digits are not part of bigger group
+#'
+#' where 00111 and 11322 are valid and 12321 is not. Function works with atomic
+#' values only
+#'
+#' @param number number to be checked
+day04_filteradj2 <- function(number) {
+  n <- nchar(number)
+  v <- sapply(
+    FUN = function(x) substr(number,x,x),
+    X = 1:n, USE.NAMES = F, simplify = T)
+  v <- c("X", v, "X")
+  checkfun <- function(pos) {
+    all(
+      v[pos-1] != v[pos],
+      v[pos+1] == v[pos],
+      v[pos+2] != v[pos]
+    )
+  }
+  if (n >= 4) {
+    b <- sapply(
+      FUN = function(pos) checkfun(pos),
+      X = 2:(length(v)-2), USE.NAMES = F, simplify = T
+    )
+    # if any of digits go in pair (no triple) - TRUE
+    any(b)
+  } else {
+    stop(paste("invalid number", number))
+  }
+
+}
+
+#' Day 04 part 1 solution
+#'
+#' @export
+day04_part2_solution <- function() {
+  ns <- strsplit(aoc19::DATASET$day04, split = "-",fixed = T)[[1]]
+  rs <- day04_makesequence(ns[1], ns[2]) %>%
+    Filter(f = day04_filterasc) %>%
+    Filter(f = day04_filteradj) %>%
+    Filter(f = day04_filteradj2)
   length(rs)
 }
--- tests/testthat/test-day04.R
+++ tests/testthat/test-day04.R
@@ -8,16 +8,32 @@ test_that("makesequence returns sequence of numbers by low and high bound", {
         "character")
 })
 
-test_that("check that digits go in ascending order", {
+test_that("digits go in ascending order", {
     expect_equal(day04_filterasc("01234"), TRUE)
     expect_equal(day04_filterasc("00000"), TRUE)
     expect_equal(day04_filterasc("01210"), FALSE)
     expect_equal(day04_filterasc("01122"), TRUE)
 })
 
-test_that("check that number has adjacent digits", {
+test_that("number has adjacent digits", {
     expect_equal(day04_filteradj("01234"), FALSE)
     expect_equal(day04_filteradj("00000"), TRUE)
     expect_equal(day04_filteradj("01210"), FALSE)
     expect_equal(day04_filteradj("01122"), TRUE)
+})
+
+test_that("the two adjacent matching digits are not part of a larger group of matching digits", {
+    expect_equal(day04_filteradj2("000000"), FALSE)
+    expect_equal(day04_filteradj2("000123"), FALSE)
+    expect_equal(day04_filteradj2("000001"), FALSE)
+    expect_equal(day04_filteradj2("001000"), TRUE)
+    expect_equal(day04_filteradj2("001113"), TRUE)
+    expect_equal(day04_filteradj2("001234"), TRUE)
+    expect_equal(day04_filteradj2("001233"), TRUE)
+    expect_equal(day04_filteradj2("666679"), FALSE)
+
+    expect_equal(day04_filteradj2("112233"), TRUE)
+    expect_equal(day04_filteradj2("123444"), FALSE)
+    expect_equal(day04_filteradj2("111122"), TRUE)
+
 })
